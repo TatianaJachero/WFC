f67dee7739a6a544a783f796746843e2af28f32b
Tatiana <pamepachojb@hotmail.com>
2022-12-21 11:45:10 -0500

    Se agregan las imagenes

--- sketch.js
+++ sketch.js
@@ -1,5 +1,64 @@
+const celdas = [];
+const RETICULA = 4;
+
+
+const azulejos = [];
+const NA = 5; //numero de azulejos
+
+
+const reglas = [
+  //reglas de los bordes de cada azulejo
+
+  {
+    //tile 0
+    UP: 0,
+    RIGHT: 0,
+    DOWN: 0,
+    LEFT: 0,
+  },
+  {
+    //tile 1
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 0,
+  },
+  {
+    //tile 2
+    UP: 0,
+    RIGHT: 1,
+    DOWN: 1,
+    LEFT: 1,
+  },
+  {
+    //tile 3
+    UP: 1,
+    RIGHT: 1,
+    DOWN: 0,
+    LEFT: 1,
+  },
+  {
+    //tile 4
+    UP: 1,
+    RIGHT: 0,
+    DOWN: 1,
+    LEFT: 1,
+  },
+];
+
+function preload() {
+  for (let i = 0; i < NA; i++) {
+    azulejos[i] = loadimage('azulejos/tile${i}.png');
+  }
+}
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(1080, 1080);
+  let opcionesI = []
+  for (let i = 0; i < azulejos.length; i++) {
+
+  }
+
 }
 
 function draw() {
